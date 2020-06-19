f68fc0d66b05b7c9bd10e9edc08c3f804c9dffc8
odoood <2523476+odoood@users.noreply.github.com>
2020-06-19 15:54:55 -0400

    Add skel distrosearch module

---
+++ distrosearch.py
@@ -0,0 +1,18 @@
+# DistroWatch Distro Package Search
+#
+# Uses the html5 package to parse the results of Package Search requests
+#
+
+def search(package, version, mode='eq'):
+    '''Perform a DistroWatch Package Search with given search mode.
+
+    Mode defaults to 'eq' and can be any of:
+      - eq (equal to)
+      - like
+      - gt (greater than)
+      - ge (greater-or-equal)
+      - lt (less than)
+      - le (less-or-equal)
+    '''
+    pass
+
