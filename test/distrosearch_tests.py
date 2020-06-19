a207e05c4eea037b0b4e8ce00cb91b50c1a7e2d4
odoood <2523476+odoood@users.noreply.github.com>
2020-06-19 18:03:55 -0400

    distrosearch: add skel unittest script

---
+++ test/distrosearch_tests.py
@@ -0,0 +1,13 @@
+# Unit tests for distrosearch module
+
+import sys
+sys.path.append('..')
+
+from distrosearch import search
+import unittest
+
+class SearchTests(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
